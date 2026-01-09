701f58c9970081853eae5a9032d1ae2801057b77
T3RR3T <t3rr3t@naver.com>
2026-01-09 11:10:52 +0900

    Delete public/js/test2.js

--- public/js/test2.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    var token = document.cookie.split('; ').find(row => row.startsWith('X-Auth-Token='));
-    if (token) {
-        alert("Token: " + token.split('=')[1]);
-    } else {
-        alert("Cookie not found or HttpOnly");
-    }
-})();
